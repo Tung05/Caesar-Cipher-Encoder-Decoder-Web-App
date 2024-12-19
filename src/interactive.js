97d94cfa43421a552eac09a972d1e590c15b1213
Tung05 <88258287+Tung05@users.noreply.github.com>
2024-12-19 00:59:35 -0500

    fixing bugs

--- src/interactive.js
+++ src/interactive.js
@@ -1,29 +1,23 @@
-// TASK #2: import the decoder class
 import decoder from "./decoderRingClass.js";
 
-// Create a decoder object.
+// Create a decoder object with an empty initial cipher
 const d = new decoder("");
 
-// TASK #3: Validate the cipher key text input
+// Validate the cipher key text input
 function validateKeyTextInput() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
   const k = document.getElementById("key");
   const check = /^[a-z]{26}$/;
   if (check.test(k.value.trim())) {
     k.style.backgroundColor = "white";
+    // Set the cipher key for the decoder
+    d.cipher = k.value.trim();
   } else {
     k.style.backgroundColor = "yellow";
   }
 }
 
-// TASK #3:  Color the encoded text box.
+// Color the encoded text box
 function colorEncodedTextBox() {
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
   const en = document.getElementById("encoded");
   if (en.value === "") {
     en.style.backgroundColor = "white";
@@ -34,22 +28,14 @@ function colorEncodedTextBox() {
   }
 }
 
-// TASK #3: Encode the text in the encode text box.
+// Encode the text in the encode text box
 function encodeText() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
   const e = document.getElementById("encode");
   document.getElementById("encoded").value = d.encode(e.value);
 }
 
-// TASK #3: Color the decoded text box.
+// Color the decoded text box
 function colorDecodedTextBox() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
   const de = document.getElementById("decoded");
   if (de.value === "") {
     de.style.backgroundColor = "white";
@@ -60,20 +46,14 @@ function colorDecodedTextBox() {
   }
 }
 
-// TASK #3: Decode the text in the decode text box.
+// Decode the text in the decode text box
 function decodeText() {
-  
-  /* BEGIN_STUDENT
-  throw new Error('Function not implemented');
-  END_STUDENT */
   const de = document.getElementById("decode");
-  document.getElementById("decoded").value = d.decode(de.value); 
+  document.getElementById("decoded").value = d.decode(de.value);
 }
 
-// TASK #4: Assign event handlers to events. Initialize background color for key
-// input.
+// Assign event handlers
 document.getElementById("key").style.backgroundColor = "yellow";
-
 document.getElementById("key").addEventListener("keyup", validateKeyTextInput);
 
 document.getElementById("encode").addEventListener("keyup", encodeText);
